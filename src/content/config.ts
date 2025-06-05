37cbc14b8daca3a3d545e9e8fdc3cbb23fecca3f
Amine <mahdiamine@outlook.com>
2025-06-05 11:44:10 +0200

    init astro projet

---
+++ src/content/config.ts
@@ -0,0 +1,10 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+    type:
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    date: z.date(),
+    tags: z.array(z.string()).optional(),
+    image: z.string().optional(),
